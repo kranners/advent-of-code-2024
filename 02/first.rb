df091b41cc5d475ac3ebe3895411ba82cbf3d8fc
Aaron Pierce <aaron@cute.engineer>
2024-12-03 16:16:53 +1100

    day two part two

--- 02/first.rb
+++ 02/first.rb
@@ -4,34 +4,50 @@ lines = File.open("./input.txt", "r").readlines
 
 def is_all_descending(record)
   record.each_cons(2) do |left, right|
-    return false if left < right
+    false if left < right
   end
 
-  return true
+  true
 end
 
 def is_all_ascending(record)
   record.each_cons(2) do |left, right|
-    return false if left > right
+    false if left > right
   end
 
-  return true
+  true
 end
 
 def is_within_range(record)
   record.each_cons(2) do |left, right|
     difference = (left - right).abs
-    return false if difference < 1
-    return false if difference > 3
+    false if difference < 1
+    false if difference > 3
   end
 
-  return true
+  true
+end
+
+def is_record_safe(record)
+  false if not is_within_range(record)
+
+  is_all_descending(record) or is_all_ascending(record)
+end
+
+def is_record_with_tolerance_safe(record)
+  true if is_record_safe(record)
+
+  record.each_with_index { |_, index| 
+    removed_record = record.dup.tap{ |i| i.delete_at(index) }
+
+    true if is_record_safe(removed_record)
+  }
+
+  false
 end
 
 records = lines.map { |line|
   line.split.map { |entry| Integer(entry) }
 }
 
-puts records.map { |record| 
-  (is_all_descending(record) || is_all_ascending(record)) && is_within_range(record)
-}.count(true)
+puts records.map { |record| is_record_with_tolerance_safe(record) }.count(true)
